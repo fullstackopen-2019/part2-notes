31ca42299da3b355416bdba69d2e476f4de3b285
Matti Luukkainen <mluukkai@iki.fi>
2019-05-30 18:15:48 +0300

    part2-1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Note from './components/Note'
 
-function App() {
+const App = ({ notes }) => {
+  const rows = () => notes.map(note =>
+    <Note
+      key={note.id}
+      note={note}
+    />
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {rows()}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App 
