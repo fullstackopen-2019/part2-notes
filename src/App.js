edc503241c4be6bf50ce9d4d736b6c8e1910a68f
Matti Luukkainen <mluukkai@iki.fi>
2019-05-31 12:35:46 +0300

    part2-4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
+import axios from 'axios'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes) 
+const App = () => {
+  const [notes, setNotes] = useState([]) 
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
 
   const notesToShow = showAll
     ? notes
