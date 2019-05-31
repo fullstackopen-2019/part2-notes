89c3501d17cf2ecd02542a2cb689adf9d12e8797
Matti Luukkainen <mluukkai@iki.fi>
2019-05-31 18:21:12 +0300

    part2-6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import axios from 'axios'
+import noteService from './services/notes' 
 
 const App = () => {
   const [notes, setNotes] = useState([]) 
@@ -8,11 +9,9 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        setNotes(response.data)
-      })
+    noteService
+      .getAll()
+      .then(initialNotes => setNotes(initialNotes))
   }, [])
 
   const notesToShow = showAll
@@ -23,6 +22,7 @@ const App = () => {
     <Note
       key={note.id}
       note={note}
+      toggleImportance={() => toggleImportanceOf(note.id)}
     />
   )
   
@@ -39,12 +39,30 @@ const App = () => {
       id: notes.length + 1,
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+    noteService
+      .create(noteObject)
+      .then(data => {
+        setNotes(notes.concat(data))
         setNewNote('')
       })
+  }
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+      
   }
 
   return (
